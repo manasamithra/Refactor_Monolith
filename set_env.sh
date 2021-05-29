be80bddf1b606cd8f46abd620ae4d0daa224f5d0
manasamithra <63588983+manasamithra@users.noreply.github.com>
2021-05-29 18:02:03 +0530

    Add files via upload

---
+++ set_env.sh
@@ -0,0 +1,11 @@
+# This file is used for convenience of local development.
+# DO NOT STORE YOUR CREDENTIALS INTO GIT
+export POSTGRES_USERNAME=udacitymanasa
+export POSTGRES_PASSWORD=udacitymanasa
+export POSTGRES_HOST=udacitymanasa.chqpzttbaybw.us-east-1.rds.amazonaws.com
+export POSTGRES_DB=postgres
+export AWS_BUCKET=manasa-udacity-dev
+export AWS_REGION=us-east-1
+export AWS_PROFILE=default
+export JWT_SECRET=hello
+export URL=http://localhost:8100
