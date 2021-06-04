5a64c8a8a301414258691d9d86e50f5b0256f35e
manasamithra <63588983+manasamithra@users.noreply.github.com>
2021-06-04 18:45:42 +0530

    Update environment.ts

--- udagram-frontend/src/environments/environment.ts
+++ udagram-frontend/src/environments/environment.ts
@@ -5,7 +5,7 @@
 export const environment = {
   production: false,
   appName: 'Udagram',
-  apiHost: 'http://localhost:8080/api/v0'
+  apiHost: 'http://FC3C3B718D8F99F497D3DCB037243637.gr7.us-east-1.eks.amazonaws.com:8080/api/v0'
 };
 
 /*
